8779da98282968dbc313b6c5c98d538e5a1b1b2c
Geree <smithzero94@gmail.com>
2023-08-05 14:33:26 +0200

    String input() metódus kész

---
+++ src/Game.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Game {
+
+
+    // beolvasás String visszatérési értékkel
+    public String input() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+}
